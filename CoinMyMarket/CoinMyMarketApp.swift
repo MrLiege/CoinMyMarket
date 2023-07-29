2cac13913101afdf1f78985136c5d4198e096d75
MrLiege <mister.4100@mail.ru>
2023-07-29 21:14:27 +0400

    Initial Commit

---
+++ CoinMyMarket/CoinMyMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinMyMarketApp.swift
+//  CoinMyMarket
+//
+//  Created by Artyom on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoinMyMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
